195f2baa81a0d0883f81871dc04ab7f3e81b36e6
Guilherme Henrique <guilherme.almeida129@etec.sp.gov.br>
2020-09-29 20:21:14 -0300

    adicionado quebra de linha

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ public class Program {
 	public static void main(String[] args) {
 		
 		System.out.println("Bom dia!");
+		System.out.println();
 		System.out.println("Hello world!");
 		System.out.println("Boa tarde");
 	}
